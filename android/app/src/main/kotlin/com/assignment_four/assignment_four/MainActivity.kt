382a1154d3b6c92ea5e5ff229eaa381ebb5323a7
himanikunwar098@gmail.com <himanikunwar098@gmail.com>
2026-05-10 16:27:40 +0545

    models

---
+++ android/app/src/main/kotlin/com/assignment_four/assignment_four/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.assignment_four.assignment_four
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/employee_model.dart
@@ -0,0 +1,17 @@
+class EmployeeModel {
+  final String id;
+  final String fullName;
+  final String gender;
+  final String department;
+  final String username;
+  final String password;
+
+  EmployeeModel({
+    required this.id,
+    required this.fullName,
+    required this.gender,
+    required this.department,
+    required this.username,
+    required this.password,
+  });
+}
